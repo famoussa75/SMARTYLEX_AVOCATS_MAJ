4bf819e908e530b222f23e4554b30715dcee8616
@cmoussa808 <cmoussa808@gmail.com>
2025-12-11 18:55:05 +0000

    correction tab pdf 10

--- resources/views/layouts/js.blade.php
+++ resources/views/layouts/js.blade.php
@@ -271,7 +271,7 @@ $(document).ready(function() {
         })
         .catch(err => console.error(err));
     @endif
-    alert("Logo Base64:", logoBase64);
+    alert("Logo Base64:", logoPath);
 
 
 
